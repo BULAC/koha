65b2da01d39697c44e085930f3599930310ffd58
Nicolas Legrand <nicolas.legrand@bulac.fr>
2017-04-06 16:43:00 +0200

    BULAC add goo to print borrower reservations

--- C4/Spaces.pm
+++ C4/Spaces.pm
@@ -10,6 +10,7 @@ BEGIN {
     @ISA       = qw(Exporter);
     @EXPORT_OK = qw(
 &GetBorrowerSpaces
+&GetBorrowerInvitations
     );
     %EXPORT_TAGS = ( 'all' => \@EXPORT_OK );
 
@@ -34,5 +35,25 @@ sub GetBorrowerSpaces {
     }
     return $retaref;
 }
+
+sub GetBorrowerInvitations {
+    my $borrower_number = shift;
+    my $query = "SELECT id_borrower, space_type.name, space_type.name as type, space.name, start_date, end_date
+                 FROM espacesprod.booking
+                 JOIN espacesprod.space ON booking.id_space = space.id_space
+                 JOIN espacesprod.space_type ON space.id_space_type = space_type.id_space_type
+                 JOIN espacesprod.invitation ON invitation.id_booking = booking.id_booking
+                 WHERE invitation.id_borrower = ?
+                 ORDER BY start_date";
+    my $dbh   = C4::Context->dbh;
+    my $sth = $dbh->prepare($query);
+    $sth->execute($borrower_number);
+    my $retaref = [];
+    while (my $row = $sth->fetchrow_hashref()) {
+	push @$retaref, $row
+    }
+    return $retaref;
+}
+
 
 1;
